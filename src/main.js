2797bf8b83d9660c868427da3c9ae277bf2ecd4a
Petrik Diak <diak@petrik.hu>
2026-01-14 12:21:40 +0100

    Majdnem kesz. WIP

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+// main.js
 
-document.querySelector('#app').innerHTML = `
+// Lekérjük az űrlapot és a siker üzenet div-et
+const form = document.querySelector('#registrationForm')
+const successDiv = document.querySelector('#successMessage')
 
-`
+// Figyeljük a submit eseményt
+form.addEventListener('submit', function(event) {
+  event.preventDefault() // Alapértelmezett elküldés megakadályozása
 
-setupCounter(document.querySelector('#counter'))
+  // Töröljük az űrlap tartalmát
+  form.style.display = 'none' 
+
+  
+  successDiv.textContent = 'Sikeres regisztráció'
+  successDiv.classList.add('alert', 'alert-success')
+})
+
+
+document.title = 'Regisztrációs oldal'
