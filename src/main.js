27b10a8170d91c27eebc0dbe4531123207f012f0
Petrik Diak <diak@petrik.hu>
2026-01-14 12:01:26 +0100

    űrlap kinézete megformázva, bootstrap, custom formázások

--- src/main.js
+++ src/main.js
@@ -4,22 +4,7 @@ import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
-  <div>
-    <form action="/action_page.php">
-  <label for="fname"><br>Keresztnév:</label><br>
 
-  <input type="text" id="fname" name="fname" value="Keresztnév"><br><br>
-
-  <label for="lname">Vezetéknév:</label><br>
-  <input type="text" id="lname" name="lname" value="Vezetéknév"><br><br>
-
-  <label for="email">E-Mail:</label><br>
-
-  <input type="text" id="email" name="email" value="email@example.com"><br><br>
-  <input type="submit" value="Beadás">
-</form> 
-    </p>
-  </div>
 `
 
 setupCounter(document.querySelector('#counter'))
